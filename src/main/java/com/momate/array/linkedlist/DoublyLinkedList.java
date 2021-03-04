c65acaed651454a96a9d961034ba28d1f157aeb9
momate <momate150@gmail.com>
2021-03-04 22:53:12 +0100

    Doubly Linked List

---
+++ src/main/java/com/momate/array/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,179 @@
+package com.momate.array.linkedlist;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedList<T> implements Iterable<T> {
+
+    private int size;
+    private Node<T> head;
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> prev;
+        private Node<T> next;
+
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public DoublyLinkedList(int size) {
+        this.size = 0;
+    }
+
+    public boolean add(T element) {
+        addFirst(element);
+        return true;
+    }
+
+
+    public void addLast(T element) {
+        if (isEmpty()) {
+            this.head = new Node(element, null, null);
+        } else {
+
+            Node last = head.next;
+            while (last.next != null) {
+                last = last.next;
+            }
+            Node<T> newNode = new Node(element, last, null);
+        }
+        ++this.size;
+    }
+
+    public void addFirst(T element) {
+        if (isEmpty()) {
+            this.head = new Node(element, null, null);
+        } else {
+            Node<T> newNode = new Node(element, null, head);
+            this.head = newNode;
+        }
+        ++this.size;
+
+    }
+
+    public T get(int index) {
+        Node<T> current = this.head;
+        while (current.next != null && index > 0) {
+            current = current.next;
+            index--;
+        }
+        return current.data;
+
+    }
+
+    public T getLast() {
+        isHeadExist();
+        if (this.size == 1) {
+            return this.head.data;
+        } else {
+            Node<T> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            return current.data;
+        }
+    }
+
+    public T getFirst() {
+        isHeadExist();
+        return this.head.data;
+    }
+
+    public T remove() {
+        return removeFirst();
+    }
+
+    public T remove(int index) {
+        if (index > size - 1 || index < 0) throw new NoSuchElementException();
+        Node<T> tmp = this.head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return removeNode(tmp);
+    }
+
+
+    public boolean remove(Object obj) {
+        Node<T> tmp = head;
+        for (tmp = head; tmp != null; tmp = tmp.next) {
+            if (obj.equals(tmp.data)) {
+                removeNode(tmp);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private T removeNode(Node<T> node) {
+        if (node.prev == null) return removeFirst();
+        if (node.next == null) return removeLast();
+
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+
+        T data = node.data;
+        //Cleanup
+        node.data = null;
+        node = node.prev = node.next = null;
+        return data;
+    }
+
+
+    public T removeFirst() {
+        isHeadExist();
+        T data = this.head.data;
+        this.head = this.head.next;
+        --size;
+        return data;
+    }
+
+    public T removeLast() {
+        isHeadExist();
+        Node<T> last = this.head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        Node<T> beforeLast = last.prev;
+        beforeLast.next = null;
+        last.prev = null;
+        --size;
+        return last.data;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private void isHeadExist() {
+        if (this.head == null) throw new NoSuchElementException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> tmp = head;
+
+            @Override
+            public boolean hasNext() {
+                return head != null;
+            }
+
+            @Override
+            public T next() {
+                T data = head.data;
+                tmp = tmp.next;
+                return data;
+            }
+        };
+    }
+
+
+}
